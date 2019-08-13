e04290345f29b76d4e4815e79c48f90b367c0800
Тетяна Пущинська <tanya.pushchinska@gmail.com>
2019-08-13 18:19:20 +0300

    Added file rubocop.yml and fixed syntax mistakes

--- app/views/railway_stations/_railway_station.json.jbuilder
+++ app/views/railway_stations/_railway_station.json.jbuilder
@@ -1,2 +1,4 @@
-json.extract! railway_station, :id, :title, :created_at, :updated_at
-json.url railway_station_url(railway_station, format: :json)
+# frozen_string_literal: true
+
+json.extract!(railway_station, :id, :title, :created_at, :updated_at)
+json.url(railway_station_url(railway_station, format: :json))
--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,21 +12,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_13_120737) do
-
+ActiveRecord::Schema.define(version: 20_190_813_120_737) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "railway_stations", force: :cascade do |t|
-    t.string "title"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'railway_stations', force: :cascade do |t|
+    t.string 'title'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "trains", force: :cascade do |t|
-    t.integer "number"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'trains', force: :cascade do |t|
+    t.integer 'number'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
-
 end

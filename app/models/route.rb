b0061222fa620b97bd15f823290a4bcbe26489f1
Tanya Pushchinska <tanya.pushchinska.com>
2019-08-28 14:27:25 +0300

    Fixed mistakes

--- app/models/route.rb
+++ app/models/route.rb
@@ -5,12 +5,4 @@ class Route < ApplicationRecord
   has_many :railway_stations, through: :railway_stations_routes
   has_many :trains
   validates :title, presence: true
-
-  def start_station
-    railway_stations.first
-  end
-
-  def end_station
-    railway_stations.last
-  end
 end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,13 +3,4 @@
 class Ticket < ApplicationRecord
   belongs_to :train
   belongs_to :railway_station
-  # validates :start_station, :end_station, :full_name, presence: true
-
-  def start_station
-    train.route.start_station
-  end
-
-  def end_station
-    train.route.end_station
-  end
 end

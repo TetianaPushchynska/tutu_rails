d47f6d623294c71984262d02bea5ced92d31ce13
Tanya Pushchinska <tanya.pushchinska.com>
2019-09-02 23:02:02 +0300

    Changed fields and strong parameters

--- app/controllers/railway_stations_controller.rb
+++ app/controllers/railway_stations_controller.rb
@@ -45,6 +45,6 @@ class RailwayStationsController < ApplicationController
   end
 
   def railway_station_params
-    params.require(:railway_station).permit(:title)
+    params.require(:railway_station).permit(:title, route_ids: [])
   end
 end
